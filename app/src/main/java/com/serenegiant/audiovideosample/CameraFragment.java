41bb4a48fd8cc94b3e3ff048b8662c9436fce1d6
saki4510t <saki4510t@gmail.com>
2020-05-02 19:14:48 +0900

    migrate to androidx

--- app/src/main/java/com/serenegiant/audiovideosample/CameraFragment.java
+++ app/src/main/java/com/serenegiant/audiovideosample/CameraFragment.java
@@ -23,7 +23,7 @@ package com.serenegiant.audiovideosample;
 */
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
--- app/src/main/java/com/serenegiant/audiovideosample/MainActivity.java
+++ app/src/main/java/com/serenegiant/audiovideosample/MainActivity.java
@@ -23,7 +23,7 @@ package com.serenegiant.audiovideosample;
 */
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
